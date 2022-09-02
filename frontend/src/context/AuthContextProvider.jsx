c8903897204c8198274d779d705600e74e5c78dd
Ratmir Kalimulin <kalimulin.91@bk.ru>
2022-09-02 16:20:40 +0300

    fix login & logout functions

--- frontend/src/context/AuthContextProvider.jsx
+++ frontend/src/context/AuthContextProvider.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import AuthContext from './AuthContext.js';
 
 function AuthContextProvider({ children }) {
+  const getUser = () => localStorage.getItem('username');
   const getToken = () => localStorage.getItem('token');
 
   const isAuthorized = () => {
@@ -10,14 +11,29 @@ function AuthContextProvider({ children }) {
   };
 
   const [authStatus, setAuthStatus] = useState(isAuthorized());
+  const toLogIn = ({ username, token }) => {
+    localStorage.setItem('token', token);
+    localStorage.setItem('username', username);
+    setAuthStatus(true);
+  };
+
+  const toLogOut = ({ username, token }) => {
+    localStorage.removeItem('token', token);
+    localStorage.removeItem('username', username);
+    setAuthStatus(false);
+  };
+
   return (
-    <AuthContext.Provider value={
-      {
-        authStatus,
-      }
+    <AuthContext.Provider value ={
+    {
+      authStatus,
+      toLogIn,
+      toLogOut,
+      getUser,
+      getToken,
+    }
     }>{children}
     </AuthContext.Provider>
   );
 }
-
 export default AuthContextProvider;
