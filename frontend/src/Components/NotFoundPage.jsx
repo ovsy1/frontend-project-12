17e8d3b0acc42e102c144ce965a773f9a00206f7
Ratmir Kalimulin <kalimulin.91@bk.ru>
2022-09-02 16:17:24 +0300

    style for 404 page

--- frontend/src/Components/NotFoundPage.jsx
+++ frontend/src/Components/NotFoundPage.jsx
@@ -5,8 +5,8 @@ const NotFoundPage = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="text-center">
-      <img alt={t('notFound.header')} className="img-thumbnail" style={{ height: '700px', 'margin-top': '100px' }} src="https://cdn2.hexlet.io/assets/error-pages/404-4b6ef16aba4c494d8101c104236304e640683fa9abdb3dd7a46cab7ad05d46e9.svg" />
+    <div className="text-center h-100 mt-5">
+      <img alt={t('notFound.header')} className="img-thumbnail" style={{ height: '700px' }} src="https://cdn2.hexlet.io/assets/error-pages/404-4b6ef16aba4c494d8101c104236304e640683fa9abdb3dd7a46cab7ad05d46e9.svg" />
       <h1 className="h4 text-muted">
         {t('notFound.header')}
       </h1>
