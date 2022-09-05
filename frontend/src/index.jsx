80677977917d57156341c1fd5d1aba9f963829e3
Ratmir Kalimulin <kalimulin.91@bk.ru>
2022-09-05 15:23:52 +0300

    init socket

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { io } from 'socket.io-client';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import init from './init.jsx';
 
 const startChat = async () => {
+  const socket = io();
   const root = ReactDOM.createRoot(document.getElementById('root'));
-  const vdom = await init();
+  const vdom = await init(socket);
 
   root.render(<React.StrictMode>{vdom}</React.StrictMode>);
 };
