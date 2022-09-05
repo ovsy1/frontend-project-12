c4ee6bbb6fc6a70f33d76cd1b0414440c9e22b9a
Ratmir Kalimulin <kalimulin.91@bk.ru>
2022-09-05 14:39:29 +0300

    add logic for Chat

--- frontend/src/Components/Chat.jsx
+++ frontend/src/Components/Chat.jsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useEffects } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Container, Row } from 'react-bootstrap';
+
+import useAuth from '../hooks/useAuth.js';
+import { loadChats } from '../features/chats-slice.js';
+import Channels from './Channels.jsx';
+import Messages from './Messages.jsx';
+import Loader from './Loader.jsx';
 
 function Chat() {
+  const dispatch = useDispatch();
+  const {
+    channels,
+    messages,
+    status,
+    error,
+  } = useSelector((state) => state.chats);
+
+  const { getHeader } = useAuth();
+
+  useEffects(() => {
+    dispatch(loadChats(getHeader));
+  }, []);
+
   return (
-    <h1>Hello home</h1>
+    <Container className="h-100 my-4 overflow-hidden rounded shadow">
+      {error && <h2>{error}</h2>}
+      {status === 'loading' && <Loader />}
+      {status === 'received' && (
+        <Row>
+          <Channels channels={channels} />
+          <Messages messages={messages} />
+        </Row>
+
+      )}
+    </Container>
   );
 }
+
 export default Chat;
