5385ee12ca06d7df7a8fef04e17422ff80af338d
Ratmir Kalimulin <kalimulin.91@bk.ru>
2022-09-05 15:54:39 +0300

    create Messages

--- frontend/src/Components/Messages.jsx
+++ frontend/src/Components/Messages.jsx
@@ -1,7 +1,79 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import {
+  Col, Form, FormControl, Row, Button,
+} from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
+import { useFormik } from 'formik';
+import { useSelector } from 'react-redux';
+
+import MessageList from './MessageList.jsx';
+import { useSocket } from '../hooks/useAuth.js';
 
 function Messages() {
-  return (<></>);
+  // const dispatch = useDispatch();
+  const { currentChannel } = useSelector((state) => state.chats);
+  const socket = useSocket();
+  const username = localStorage.getItem('username');
+  const { t } = useTranslation();
+  const textInput = useRef(null);
+
+  useEffect(() => {
+    textInput.current.focus();
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      message: '',
+    },
+    onSubmit: (values) => {
+      const newMessage = {
+        message: values.message,
+        channelId: currentChannel,
+        username,
+      };
+
+      socket.addNewMessage(newMessage, (response) => {
+        console.log(response, newMessage);
+      });
+
+      formik.resetForm();
+    },
+  });
+
+  return (
+    <Col className="d-flex flex-column h-100 p-0">
+      <MessageList />
+      <div className="mt-auto px-5 py-3">
+      <Form onSubmit={formik.handleSubmit} className="form-label">
+      <Row className="align-items-center">
+        <Col className="p-0">
+          <FormControl
+            ref={textInput}
+            data-testid="new-message"
+            placeholder={t('messages.placeholderName')}
+            name="message"
+            type="text"
+            value={formik.values.message}
+            onChange={formik.handleChange}
+            disabled={formik.isSubmitting}
+          />
+        </Col>
+        <Col xs="auto">
+          <Button
+            role="button"
+            className="btn btn-group-vertical"
+            type="submit"
+            name={t('messages.send')}
+            disabled={formik.values.message === '' || formik.isSubmitting}
+          >
+            {t('messages.send')}
+          </Button>
+        </Col>
+      </Row>
+    </Form>
+      </div>
+    </Col>
+  );
 }
 
 export default Messages;
