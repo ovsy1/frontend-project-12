4f46e834c6371f128a0cb9da171f098f2f0d116a
Ratmir Kalimulin <kalimulin.91@bk.ru>
2022-09-05 14:05:25 +0300

    use redux provider

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import AuthContextProvider from './context/AuthContextProvider.jsx';
+import store from './store.js';
 
 import languages from './locales/index.js';
 import App from './Components/App.jsx';
@@ -10,20 +12,20 @@ import App from './Components/App.jsx';
 export default async () => {
   const i18n = i18next.createInstance();
 
-  await i18n
-    .use(initReactI18next)
-    .init({
-      resources: languages,
-      fallbackLng: 'ru',
-    });
+  await i18n.use(initReactI18next).init({
+    resources: languages,
+    fallbackLng: 'ru',
+  });
 
   const vdom = (
     <I18nextProvider>
-      <BrowserRouter>
-        <AuthContextProvider>
-          <App />
-        </AuthContextProvider>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <AuthContextProvider>
+            <App />
+          </AuthContextProvider>
+        </BrowserRouter>
+      </Provider>
     </I18nextProvider>
   );
 
