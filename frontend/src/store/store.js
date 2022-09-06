0b7bd53e122819e9cb934a59313fd5fe4a4130ab
Ratmir Kalimulin <kalimulin.91@bk.ru>
2022-09-06 15:28:59 +0300

    add modal reducer in store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import chatsReducer from './features/chats-slice.js';
+import modalReducer from './features/modal-slice.js';
 
 const store = configureStore({
   reducer: {
     chats: chatsReducer,
+    modal: modalReducer,
   },
   devTools: true,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
