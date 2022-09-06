cf3fb50c6b69a7ea9eaa068c01be62f9a0e02c0d
Ratmir Kalimulin <kalimulin.91@bk.ru>
2022-09-06 15:06:15 +0300

    add logic modal

--- frontend/src/Components/Channel.jsx
+++ frontend/src/Components/Channel.jsx
@@ -3,10 +3,28 @@ import {
   Button, ButtonGroup, Dropdown, Nav,
 } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+import { setActiveChannel } from '../store/features/chats-slice.js';
+import { setModal } from '../store/features/modal-slice.js';
 
-function Channel({ channel }) {
+function Channel({ channel, setRemoveModal, setRenameModal }) {
+  const dispatch = useDispatch();
+  const { currentChannel } = useSelector((state) => state.chats);
+
   const { name, id } = channel;
   const { t } = useTranslation();
+
+  const buttonStyle = currentChannel === id && 'primary';
+
+  const handleShowRemove = () => {
+    setRemoveModal(true);
+    dispatch(setModal({ name, id }));
+  };
+
+  const handleShowRename = () => {
+    setRenameModal(true);
+    dispatch(setModal({ name, id }));
+  };
 
   return (
     <Nav.Item key={id} as="li" className="w-100">
@@ -16,6 +34,8 @@ function Channel({ channel }) {
 
           type="button"
           className="w-100 rounded-0 text-start text-truncate my-1"
+          variant={buttonStyle}
+          onClick={() => dispatch(setActiveChannel(id))}
         >
           <span># </span>
           {name}
@@ -24,12 +44,17 @@ function Channel({ channel }) {
           aria-haspopup="true"
           split
           className="flex-grow-0 my-1 rounded-end"
+          variant={buttonStyle}
         />
         <Dropdown.Menu>
-          <Dropdown.Item >
+          <Dropdown.Item
+          onClick={handleShowRemove}
+          >
             {t('channels.remove')}
           </Dropdown.Item>
-          <Dropdown.Item >
+          <Dropdown.Item
+          onClick={handleShowRename}
+          >
             {t('channels.rename')}
           </Dropdown.Item>
         </Dropdown.Menu>
