6d11253e8541b6864237836ae354386c3e1b2c25
Ratmir Kalimulin <kalimulin.91@bk.ru>
2022-09-06 14:15:36 +0300

    create ref for last message

--- frontend/src/Components/MessageList.jsx
+++ frontend/src/Components/MessageList.jsx
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 
 function MessageList() {
   const { messages, currentChannel } = useSelector((state) => state.chats);
+  const messageEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messageEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
   return (
     <div
-    className="chat-messages h-100 overflow-auto text-break px-5"
+    className="h-100 chat-messages overflow-auto text-break px-5"
     id="messages-box"
   >
     {
-    messages
-      .filter((message) => message.channelId === currentChannel)
-      .map((message) => <div key={message.id} className="text-break mb-2">
-    <b>{message.username}</b>
-    {':'} {message.message}
-  </div>)
-  }
+      messages
+        .filter((message) => message.channelId === currentChannel)
+        .map((message) => <div key={message.id} className="text-break mb-2">
+      <b>{message.username}</b>
+      {':'} {message.message}
+      <div ref={messageEndRef} />
+      </div>)
+    }
   </div>
   );
 }
