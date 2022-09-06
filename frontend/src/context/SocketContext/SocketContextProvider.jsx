113b8cdd24584da31d53a01fbce0f9fc08a89fd2
Ratmir Kalimulin <kalimulin.91@bk.ru>
2022-09-06 15:26:07 +0300

    change socket context logic

--- frontend/src/context/SocketContext/SocketContextProvider.jsx
+++ frontend/src/context/SocketContext/SocketContextProvider.jsx
@@ -2,35 +2,35 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 
 import SocketContext from './SocketContext';
-import { addMessage } from '../../store/features/chats-slice.js';
+import {
+  addMessage, addChannel, removeChannel, renameChannel,
+} from '../../store/features/chats-slice.js';
 
 function SocketContextProvider({ children, socket }) {
   const dispatch = useDispatch();
 
   socket.on('newMessage', (message) => {
-    console.log(message);
     dispatch(addMessage(message));
   });
   socket.on('newChannel', (channel) => {
-    console.log(channel);
-  });
-  socket.on('renameChannel', (name) => {
-    console.log(name);
+    dispatch(addChannel(channel));
   });
   socket.on('removeChannel', (id) => {
-    console.log(id);
+    dispatch(removeChannel(id));
+  });
+  socket.on('renameChannel', (data) => {
+    dispatch(renameChannel(data));
   });
 
   const socketValues = {
     addNewMessage: (message, response) => socket.emit('newMessage', message, response),
     newChannel: (channel, response) => socket.emit('newChannel', channel, response),
-    renameChannel: (name, response) => socket.emit('renameChannel', name, response),
     removeChannel: (id, response) => socket.emit('removeChannel', id, response),
+    renameChannel: (data, response) => socket.emit('renameChannel', data, response),
   };
 
   return (
     <SocketContext.Provider value={ socketValues }>{children}</SocketContext.Provider>
   );
 }
-
 export default SocketContextProvider;
