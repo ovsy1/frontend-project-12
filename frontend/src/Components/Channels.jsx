4da42eca47b6d9f4fd346253cbf5dd92b120654e
Ratmir Kalimulin <kalimulin.91@bk.ru>
2022-09-06 15:19:52 +0300

    create state for modal

--- frontend/src/Components/Channels.jsx
+++ frontend/src/Components/Channels.jsx
@@ -6,10 +6,14 @@ import _ from 'lodash';
 
 import Channel from './Channel.jsx';
 import RemovableChannel from './RemovableChannel.jsx';
-import AddChannel from './Modal/AddChannel.jsx';
+import AddChannel from './Modal/Add/AddChannel.jsx';
+import RemoveChannel from './Modal/Remove/RemoveChannel.jsx';
+import RenameChannel from './Modal/Rename/RenameChannel.jsx';
 
 function Channels() {
   const [showModal, setShowModal] = useState(false);
+  const [removeModal, setRemoveModal] = useState(false);
+  const [renameModal, setRenameModal] = useState(false);
   const { channels } = useSelector((state) => state.chats);
   const { t } = useTranslation();
 
@@ -34,11 +38,13 @@ function Channels() {
     <div id="channel-list">
       <Nav fill as="ul" variant="pills" className="flex-column px-2">
         {channels.map((channel) => (channel.removable
-          ? <Channel key={_.uniqueId('Channel_')} channel={channel}/>
+          ? <Channel key={_.uniqueId('Channel_')} channel={channel} setRemoveModal={setRemoveModal} setRenameModal={setRenameModal}/>
           : <RemovableChannel key ={_.uniqueId('RemovableChannel')} channel={channel}/>))}
       </Nav>
     </div>
-    {showModal ? <AddChannel setShowModal={setShowModal} showModal={showModal}/> : null}
+    {showModal && <AddChannel setShowModal={setShowModal} showModal={showModal}/>}
+    {removeModal && <RemoveChannel removeModal={removeModal} setRemoveModal={setRemoveModal}/>}
+    {renameModal && <RenameChannel renameModal={renameModal} setRenameModal={setRenameModal}/>}
   </Col>
   );
 }
