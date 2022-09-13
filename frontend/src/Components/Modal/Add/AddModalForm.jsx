12736701e36027668bfab01289eebdf2f686724b
Ratmir Kalimulin <kalimulin.91@bk.ru>
2022-09-13 18:17:27 +0300

    fix label

--- frontend/src/Components/Modal/Add/AddModalForm.jsx
+++ frontend/src/Components/Modal/Add/AddModalForm.jsx
@@ -52,7 +52,7 @@ function AddModalForm({ setShowModal }) {
           disabled={formik.isSubmitting}
           isInvalid={formik.errors.name}
         />
-        <label htmlFor="AddedForm">{t('modals.nameChannel')}</label>
+        <label className='visually-hidden' htmlFor="AddedForm">{t('modals.nameChannel')}</label>
         <Form.Control.Feedback type="invalid">
           {t(formik.errors.name)}
         </Form.Control.Feedback>
--- frontend/src/Components/Modal/Rename/RenameModalForm.jsx
+++ frontend/src/Components/Modal/Rename/RenameModalForm.jsx
@@ -47,32 +47,32 @@ function RenameModalForm({ setRenameModal }) {
     <Form onSubmit={formik.handleSubmit}>
       <Form.Group>
         <Form.Control
-          name="name"
-          className="mb-2"
-          testid="rename-channel"
+          name='name'
+          className='mb-2'
+          testid='rename-channel'
           onChange={formik.handleChange}
           ref={textInput}
           disabled={formik.isSubmitting}
           isInvalid={formik.errors.name}
           value={formik.values.name || targetModalName}
         />
-        <label htmlFor="AddedForm">{t('modals.nameChannel')}</label>
-        <Form.Control.Feedback type="invalid">
+        <label className='visually-hidden' htmlFor='AddedForm'>{t('modals.nameChannel')}</label>
+        <Form.Control.Feedback type='invalid'>
           {t(formik.errors.name)}
         </Form.Control.Feedback>
-        <div className="d-flex justify-content-end">
+        <div className='d-flex justify-content-end'>
           <Button
             onClick={() => setRenameModal(false)}
-            type="button"
+            type='button'
             name={t('modals.cancel')}
-            className="me-2 btn btn-secondary"
+            className='me-2 btn btn-secondary'
           >
             {t('modals.cancel')}
           </Button>
           <Button
-            type="submit"
+            type='submit'
             name={t('modals.send')}
-            className="btn btn-primary"
+            className='btn btn-primary'
           >
             {t('modals.send')}
           </Button>
