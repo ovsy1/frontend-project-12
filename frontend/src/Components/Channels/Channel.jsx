2a56eae350388e1c0dc268e9cbbef4516c858ec2
Ratmir Kalimulin <kalimulin.91@bk.ru>
2022-09-14 10:32:23 +0300

    fix

--- frontend/src/Components/Channels/Channel.jsx
+++ frontend/src/Components/Channels/Channel.jsx
@@ -41,9 +41,7 @@ function Channel({ channel, setRemoveModal, setRenameModal }) {
           className="flex-grow-0 my-1 rounded-end"
           variant={buttonStyle}
         />
-        <Dropdown.Toggle split className="flex-grow-0" variant={buttonStyle}>
-              <span className="visually-hidden">{t('channels.manage')}</span>
-          </Dropdown.Toggle>
+          <span className="visually-hidden">{t('channels.manage')}</span>
         <Dropdown.Menu>
           <Dropdown.Item
           onClick={handleShowRemove}
