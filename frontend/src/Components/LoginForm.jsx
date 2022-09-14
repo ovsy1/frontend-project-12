ff6e0e9d02c401ba341275aef91fb3788f512df3
Ratmir Kalimulin <kalimulin.91@bk.ru>
2022-09-14 14:14:38 +0300

    Fix

--- frontend/src/Components/LoginForm.jsx
+++ frontend/src/Components/LoginForm.jsx
@@ -52,38 +52,38 @@ function LoginForm() {
 
   return (
     <Form onSubmit={formik.handleSubmit} className="col-12 col-md-6 mt-3 mt-mb-0">
-      <h1 className="text-center mb-4">{t('login.header')}</h1>
-      <Form.Group className="form-floating mb-3">
-        <Form.Control
-        onChange={formik.handleChange}
-        value={formik.values.username}
-        name="username"
-        id="username"
-        autoComplete="username"
-        isInvalid={authFailed}
-        required
-        ref={inputRef}
-        placeholder={t('login.username')}
-        />
-        <label htmlFor="username">{t('login.username')}</label>
-      </Form.Group>
-      <Form.Group className="form-floating mb-4">
-        <Form.Control
-        type="password"
-        onChange={formik.handleChange}
-        value={formik.values.password}
-        name="password"
-        id="password"
-        autoComplete="current-password"
-        isInvalid={authFailed}
-        required
-        placeholder={t('login.password')}
-        />
-        <Form.Label htmlFor="password">{t('login.password')}</Form.Label>
-        {authFailed && <Form.Control.Feedback type="invalid" tooltip>{t('login.authFailed')}</Form.Control.Feedback>}
-      </Form.Group>
-      <Button type="submit" variant="outline-primary" className="w-100 mb-3">{t('login.submit')}</Button>
-    </Form>
+                <h1 className="text-center mb-4">{t('login.header')}</h1>
+                <Form.Group className="form-floating mb-3">
+                  <Form.Control
+                    onChange={formik.handleChange}
+                    value={formik.values.username}
+                    name="username"
+                    id="username"
+                    autoComplete="username"
+                    isInvalid={authFailed}
+                    required
+                    ref={inputRef}
+                    placeholder={t('login.username')}
+                  />
+                  <label htmlFor="username">{t('login.username')}</label>
+                </Form.Group>
+                <Form.Group className="form-floating mb-4">
+                  <Form.Control
+                    type="password"
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                    name="password"
+                    id="password"
+                    autoComplete="current-password"
+                    isInvalid={authFailed}
+                    required
+                    placeholder={t('login.password')}
+                  />
+                  <Form.Label htmlFor="password">{t('login.password')}</Form.Label>
+                  {authFailed && <Form.Control.Feedback type="invalid" tooltip>{t('login.authFailed')}</Form.Control.Feedback>}
+                </Form.Group>
+                <Button type="submit" variant="outline-primary" className="w-100 mb-3">{t('login.submit')}</Button>
+              </Form>
   );
 }
 
