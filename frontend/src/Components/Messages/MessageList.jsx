027bb4c612a47f5c1777353ab16508a617bcf948
Ratmir Kalimulin <kalimulin.91@bk.ru>
2022-09-15 12:15:26 +0300

    fix linter

--- frontend/src/Components/Messages/MessageList.jsx
+++ frontend/src/Components/Messages/MessageList.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-curly-brace-presence */
+/* eslint-disable react/jsx-wrap-multilines */
 import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
 
@@ -18,14 +20,14 @@ function MessageList() {
       className="h-100 chat-messages overflow-auto text-break px-5"
       id="messages-box"
     >
-      {(
+      {
         messages.filter((message) => message.channelId === currentChannel).map((message) => <div key={message.id} className="text-break mb-2">
           <b>{message.username}</b>
           {':'}
           {message.message}
           <div ref={messageEndRef} />
         </div>)
-      )}
+      }
     </div>
   );
 }
