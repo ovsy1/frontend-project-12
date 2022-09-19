841e0ae9ee2375e979dd7e77e031cfb4d45344b1
Ratmir Kalimulin <kalimulin.91@bk.ru>
2022-09-19 11:07:57 +0300

    add path to routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,6 +1,9 @@
 const api = '/api/v1';
 
 export default {
+  channelsPath: () => [api, 'channels'].join('/'),
+  channelPath: (id) => [api, 'channels', id].join('/'),
+  channelMessagesPath: (id) => [api, 'channels', id, 'messages'].join('/'),
   loginPath: () => [api, 'login'].join('/'),
   signupPagePath: () => [api, 'signup'].join('/'),
   mainPagePath: () => [api, 'data'].join('/'),
